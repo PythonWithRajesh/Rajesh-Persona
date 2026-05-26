cef2a54806b45f1862cdf22ab95ebbc23ce69632
codewithrajesh <rajeshlagdhir2212@gmail.com>
2026-05-26 16:05:53 +0530

    fix allowed hosts for render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-1=iz90sjd*-5bw3jat*m)&(5ml!a!t(c@+e93x72dpei%6&!g0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'rajesh-persona.onrender.com',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
