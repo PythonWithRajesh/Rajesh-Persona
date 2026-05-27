a543c18005b0e43fe4c1966331cd54b589d74157
codewithrajesh <rajeshlagdhir2212@gmail.com>
2026-05-27 19:53:52 +0530

    fixed contact form

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,4 +142,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rajeshlagdhir07@gmail.com'
-EMAIL_HOST_PASSWORD = 'eneq nqyy njbw kgbp'
+EMAIL_HOST_PASSWORD = 'eneqnqyynjbwkgbp'
