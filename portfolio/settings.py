b3c5d9a68a72c138983e45fe3bf56af98fdbf3da
codewithrajesh <rajeshlagdhir2212@gmail.com>
2026-05-29 11:40:18 +0530

    fixed contact form

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("rajeshlagdhir07@gmail.com")
-EMAIL_HOST_PASSWORD = os.environ.get("zkvvnxksvqdfvznd")
+EMAIL_HOST_USER = 'rajeshlagdhir07@gmail.com'
+EMAIL_HOST_PASSWORD = 'zkvv nxks vqdf vznd'
